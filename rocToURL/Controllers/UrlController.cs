6db50f651194f68e8d649ed886885dd36fb39e88
Andrew Kennedy <akennedy010@gmail.com>
2022-03-08 14:54:25 +0000

    add boiler plate code as base for URL shorteners

---
+++ rocToURL/Controllers/UrlController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using rocToURL.Models;
+
+namespace rocToURL.Controllers
+{
+    public class UrlController : Controller
+    {
+        [HttpGet]
+        public IActionResult Shorten()
+        {
+            return View();
+        }
+
+        public ActionResult Shorten(URL url)
+        {
+            // checks if all validation requirements are met
+            if (ModelState.IsValid)
+            {
+                url.ShortUrl = String.Empty;
+            }
+
+            return View(url);
+        }
+    }
+}
--- rocToURL/Models/URL.cs
+++ rocToURL/Models/URL.cs
@@ -1,10 +1,16 @@
-﻿namespace rocToURL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rocToURL.Models
 {
     public class URL
     {
-        public Guid Id { get; set; }    
-        public string LongURL { get; set; }
+        public Guid Id { get; set; }
+
+        [Required]
+        public string LongUrl { get; set; }
+
         public string ShortUrl { get; set; }
+
         public DateTime CreatedDate { get; set; }
     }
 }
