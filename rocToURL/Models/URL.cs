28dde87fd9341d6381ae0f99b7ccf9982093a336
Andrew Kennedy <akennedy010@gmail.com>
2022-03-08 10:24:39 +0000

    Reconfigure to MVC as it was created incorrectly at the beginning

---
+++ rocToURL/Models/URL.cs
@@ -0,0 +1,10 @@
+﻿namespace rocToURL.Models
+{
+    public class URL
+    {
+        public Guid Id { get; set; }    
+        public string LongURL { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
