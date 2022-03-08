c45913b27b3e7f13baba37f2a8e3b53ade05587e
Andrew Kennedy <akennedy010@gmail.com>
2022-03-08 15:18:23 +0000

    Create class library for interfaces and move IURL interface over. Create URL service boiler plate

---
+++ rocToURL.Abstractions/IUrlService.cs
@@ -0,0 +1,15 @@
+﻿namespace rocToURL.Abstractions
+{
+    /// <summary>
+	/// Url Service
+	/// </summary>
+    public interface IUrlService
+    {
+        /// <summary>
+		/// Minify a Url string 
+		/// </summary>
+		/// <param name="longUrl">The long url to minimise</param>
+		/// <returns>The minimised short url</returns>
+        Task<string> MinifyUrl(string longUrl);
+    }
+}
---
+++ rocToURL/Services/UrlServiceImplementation.cs
@@ -0,0 +1,16 @@
+﻿using rocToURL.Abstractions;
+
+namespace rocToURL.Services
+{
+    public class UrlServiceImplementation : IUrlService
+    {
+        /// <inheritdoc/>
+        public Task<string> MinifyUrl(string longUrl)
+        {
+            return Task.Run(() =>
+            {
+                return "Not yet implemented";
+            });
+        }
+    }
+}
